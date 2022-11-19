011510508110cb79abd2a13e2f27bc7ee195f8e1
Mohamed <d2105944@hotmail.com>
2022-11-19 15:05:22 +0300

    first missing positive

---
+++ 41- FirstMissingPositive.java
@@ -0,0 +1,29 @@
+
+public class answer {
+
+	public static void main(String[] args) {
+		System.out.println(firstMissingPositive(new int[] {3,4,-1,1}));
+	}
+	
+	static int firstMissingPositive(int[] nums) {
+		int i = 0;
+		while(i < nums.length) {
+			
+			if (nums[i] == i + 1 || nums[i] <= 0 || nums[i] > nums.length) i++;    // check if element
+			
+			else if (nums[nums[i] - 1] != nums[i]) swap(nums, i, nums[i] - 1);    // put element in its correct position
+			
+			else i++;
+		}
+		
+		i = 0;
+		while (i < nums.length && nums[i] == i + 1) i++;
+		return i + 1;
+	}
+	
+	static void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+}
