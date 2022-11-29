c98a065c753c1a3589f3b0cecce1e55000f5b0c0
Mohamed <d2105944@hotmail.com>
2022-11-29 17:44:35 +0300

    merge intervals

---
+++ 56-MergeIntervals.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class answer {
+
+	public static void main(String[] args) {
+		int[][] res = merge(new int[][] {{1,3},{2,6},{8,10},{15,18}});
+	}
+	
+    static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        
+        for (int[] interval : intervals){
+        	
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0])
+                merged.add(interval);
+            else
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
