0916481d685fd660b636930fe1155a223ad92c6a
Mohamed <d2105944@hotmail.com>
2022-12-29 14:05:49 +0300

    maximum product subarray

---
+++ 152-MaximumProductSubarray.java
@@ -0,0 +1,23 @@
+
+public class solution {
+
+	public static void main(String[] args) {
+		System.out.println(maxProduct(new int[] {2,3,-2,-4}));
+	}
+	
+	static int maxProduct(int[] nums) {
+		if (nums == null || nums.length == 0) return 0;
+		
+		int max = nums[0], min = nums[0], res = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			int temp = max;
+			
+			max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+			min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+			
+			if (max > res) res = max;
+		}
+		return res;
+	}
+
+}
