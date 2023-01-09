efe455763d0c7b2aab15883c7e4968f72cd314d3
Mohamed <d2105944@hotmail.com>
2023-01-09 16:58:08 +0300

    kth largest element in an array

---
+++ 215-KthLargestElementInAnArray.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+public class solution {
+
+	public static void main(String[] args) {
+		System.out.println(findKthLargest(new int[] {3,2,1,5,6,4}, 2));
+	}
+	
+    static int findKthLargest(int[] nums, int k) {
+        shuffle(nums);
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    static void shuffle(int a[]) {
+        final Random random = new Random();
+        for(int ind = 1; ind < a.length; ind++) {
+            final int r = random.nextInt(ind + 1);
+            exch(a, ind, r);
+        }
+    }
+
+    static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    static void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    static boolean less(int v, int w) {
+        return v < w;
+    }
+
+}
