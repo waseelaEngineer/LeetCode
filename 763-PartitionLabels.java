84b2257318c012187ca81e16b748c85e0c0ad6b7
Mohamed <d2105944@hotmail.com>
2023-01-31 11:44:38 +0300

    greedy

---
+++ 763-PartitionLabels.java
@@ -0,0 +1,29 @@
+// Time: O(n)
+// Space: O(1)
+import java.util.ArrayList;
+import java.util.List;
+
+public class solution {
+
+	public static void main(String[] args) {
+		System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+	}
+		
+	static List<Integer> partitionLabels(String S) {
+	    int[] last = new int[26];
+	    for (int i = 0; i < S.length(); ++i)
+	        last[S.charAt(i) - 'a'] = i;
+	    
+	    int j = 0, anchor = 0;
+	    List<Integer> ans = new ArrayList();
+	    for (int i = 0; i < S.length(); ++i) {
+	        j = Math.max(j, last[S.charAt(i) - 'a']);
+	        if (i == j) {
+	            ans.add(i - anchor + 1);
+	            anchor = i + 1;
+	        }
+	    }
+	    return ans;
+	}
+
+}
